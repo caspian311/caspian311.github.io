efbd99b6445da4fa4e6ecd57cdc0434ec9336dcc
Matt Todd <matt.c.todd@gmail.com>
2014-06-26 06:35:14 -0500

    green borders

--- js/constants.js
+++ js/constants.js
@@ -9,7 +9,7 @@ define(function() {
       NUMBER_OF_CELLS_WIDTH: lengthCellCount,
       WHITE: "rgb(255, 255, 255)",
       BLACK: "rgb(0, 0, 0)",
-      RED: "rgb(0, 0, 255)"
+      RED: "rgb(0, 100, 0)"
    }
    Constants.WIDTH_OF_CELL = Math.floor(Constants.SCREEN_WIDTH / Constants.NUMBER_OF_CELLS_WIDTH);
    Constants.HEIGHT_OF_CELL = Math.floor(Constants.SCREEN_HEIGHT / Constants.NUMBER_OF_CELLS_HEIGHT);
